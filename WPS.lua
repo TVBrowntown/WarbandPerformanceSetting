123617923fcdda1f7120a78c06ed8a55fee5ede6
Bset <lysander__@hotmail.com>
2021-03-08 10:09:15 +1030

    fix
    leaving party was triggering a check twice resulting in not returning to original video settings.
    
    removed toggle function as we will not be using it.

--- WPS.lua
+++ WPS.lua
@@ -4,6 +4,8 @@
  Custom 1 = Not in a Warband
  Low = In a Warband
 
+GetNumGroupmates()
+
 ]]-- 
 
 WPS = {}
@@ -16,7 +18,6 @@ function WPS.OnInit()
   RegisterEventHandler(SystemData.Events.GROUP_ACCEPT_REFERRAL, "WPS.Join")
   RegisterEventHandler(SystemData.Events.GROUP_LEAVE, "WPS.Leave")
   RegisterEventHandler(SystemData.Events.BATTLEGROUP_UPDATED, "WPS.Join")
-  RegisterEventHandler(SystemData.Events.BATTLEGROUP_MEMBER_UPDATED, "WPS.Join")
 end
 
 function WPS.Join()
@@ -29,18 +30,6 @@ end
 
 function WPS.Leave()
   if SystemData.Settings.Performance.perfLevel ~= SoloSetting then
-    SystemData.Settings.Performance.perfLevel = SoloSetting
-    BroadcastEvent(SystemData.Events.USER_SETTINGS_CHANGED)
-    d("SoloSetting (PERF_LEVEL_CUSTOM1) applied!")
-  end
-end
-
-function WPS.Toggle()
-  if SystemData.Settings.Performance.perfLevel ~= WarbandSetting then
-    SystemData.Settings.Performance.perfLevel = WarbandSetting
-    BroadcastEvent(SystemData.Events.USER_SETTINGS_CHANGED)
-    d("WarbandSetting (PERF_LEVEL_LOW) applied!")
-  else
     SystemData.Settings.Performance.perfLevel = SoloSetting
     BroadcastEvent(SystemData.Events.USER_SETTINGS_CHANGED)
     d("SoloSetting (PERF_LEVEL_CUSTOM1) applied!")
